17b1817270563569568d115ece1a786cb06cd8d5
FedoseevaAlexandra <70970063+FedoseevaAlexandra@users.noreply.github.com>
2021-12-17 20:08:15 +0300

    Update list.3.py

--- list.3.py
+++ list.3.py
@@ -1,12 +1,7 @@
-x=''
-with open ('input.txt','r') as f:
-	while True:
-	   l=f.readline()
-	   x+=l
-	   if len(l)==0:
-		   break
-y=list(x.split('\n'))
-y.sort()
-z=str(y)
-with open('output.txt','w') as f:
-   f.write(z)
+with open ('C:/Users/moonb/OneDrive/Desktop/input.txt','r') as f:
+	l=f.read()
+L=[]
+L.extend(l)
+L.sort()
+with open('C:/Users/moonb/OneDrive/Desktop/output.txt','w') as f:
+   f.write(''.join(L))

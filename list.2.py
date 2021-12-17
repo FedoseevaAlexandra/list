a075a87e91b6bb4b5952bef0d4fd50d3fa08941b
FedoseevaAlexandra <70970063+FedoseevaAlexandra@users.noreply.github.com>
2021-12-17 21:22:45 +0300

    Update list.2.py

--- list.2.py
+++ list.2.py
@@ -54,4 +54,46 @@ with open ('output2.txt','w') as f:
    f.write(x)
 print(x)
 
+#Se citesc elementele unei liste, care reprezintă numere întregi (pozitive și negative). Să se afișeze la ecran:
+	#a) conținutul (elementele) listei /lista1
+	#b) lista sortată în ordine crescătoare / lista2
+	#c) lista sortată în ordine descrescătoare / lista3
+	#d) numărul de elemente din listă
+	#e) elementul MAX
+	#f) elementul MIN
+	#g) să se adauge la coadă în lista inițială elementul – 111. 
+	   # Să se afișeze lista nou-formată. / lista4
+	#h) să se adauge pe poziția a doua din lista inițială elementul – 222. 
+	   # Să se afișeze lista nou-formată. / lista5
+with open ('C:/Users/moonb/OneDrive/Desktop/input.txt','r') as f:
+	y=f.read()
+y=y.split(' ')
+y.remove('')
+l2=len(y)
+l4=max(y)
+l5=min(y)
+l1=sorted(int(i) for i in y)
+print(l1)
+l3=l1[::-1]
+print(type(y))
+lj=y.append('111')
+
+#l3=l.sort(reverse=True) nu lucreaza
+l1=str(l1)
+l3=str(l3)
+lj=str(lj)
+l2=str(l2)
+l4=str(l4)
+l5=str(l5)
+t=y
+y=str(y)
+wr=['continutul listei\n',y,'\nlista sortata in ordine crescatoare\n',str(l1),'\nlista sortata in ordine descrescatoare\n',str(l3),'\nnumarul de elemente din lista=\n',l2,'\nelementul MAX=\n',str(l4),'\nelementul MIN=\n',l5,'\nlista initiala elementul+111\n',lj]
+with open ('C:/Users/moonb/OneDrive/Desktop/output.txt','w') as f:
+   f.writelines(wr)
+t[1]='222'
+with open ('C:/Users/moonb/OneDrive/Desktop/output.txt','a') as f:
+   f.write('\nlista initiala elementul+222\n')
+   f.write(str(t))
+print(t)
+
         
